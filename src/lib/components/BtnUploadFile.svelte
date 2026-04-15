332b53a2aaf6335708f62e6fdaa81dd5bce9c44b
alt1tla <GerdzhikovaVeronWench@gmail.com>
2026-04-15 16:49:01 +0300

    🎉 chore: svelte-heros-v2 replace with phosphor-svelte, add favicon

--- src/lib/components/BtnUploadFile.svelte
+++ src/lib/components/BtnUploadFile.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { Button } from "bits-ui";
-  import { Photo } from "svelte-heros-v2";
+  import Image from "phosphor-svelte/lib/Image";
   import { imageFile } from "$lib/stores/image";
+  import { saveImage } from "$lib/stores/saveImage"
   import { goto } from "$app/navigation";
   
   let { topage = null } = $props();
@@ -11,10 +12,11 @@
     input.click();
   }
 
-  function handleFile(e: Event) {
+  async function handleFile(e: Event) {
     const file = (e.target as HTMLInputElement).files?.[0];
     if (!file) return;
     imageFile.set(file);
+    await saveImage(file);
     if (topage) {
       goto(topage);
     }
@@ -23,9 +25,9 @@
 
 <Button.Root
   onclick={openFileDialog}
-  class="cursor-pointer bg-gray-800 text-gray-100 hover:bg-gray-700 px-4 py-2 rounded-4xl flex items-center gap-2"
+  class="cursor-pointer bg-gray-800 text-gray-100 hover:bg-gray-700 px-4 py-2 flex items-center gap-2"
 >
-  <Photo class="w-5 h-5" />
+  <Image class="size-6" />
   Загрузить изображение
 </Button.Root>
 
