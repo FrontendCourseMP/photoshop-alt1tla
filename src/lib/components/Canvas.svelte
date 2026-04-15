7f17bff46d251fcdd0c9ff9779f5f82d7f53ed86
alt1tla <GerdzhikovaVeronWench@gmail.com>
2026-04-15 17:15:48 +0300

    ✨ feat(canvas): update  with IDB & imageInfo

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -1,83 +1,78 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { get } from "svelte/store";
+  import { onMount, onDestroy } from "svelte";
   import { imageFile } from "$lib/stores/image";
-  import type { ImageInfo } from "$lib/types/image";
-
-  let { onInfo }: { onInfo: (data: ImageInfo) => void } = $props();
-
+  import { imageInfo } from "$lib/stores/imageInfo";
+  import { loadImage } from "$lib/stores/loadImage";
+  
   let canvas: HTMLCanvasElement;
   let ctx: CanvasRenderingContext2D;
   let img: HTMLImageElement | null = null;
+ 
+  let unsubscribe: (() => void) | undefined;
 
+  // Размеры 
   let width = 0;
   let height = 0;
-
   // Смещение для перемещения изображения
   let offsetX = 0;
   let offsetY = 0;
   // Флаг для перетаскивания
   let isDragging = false;
-  // Начальные координаты при начале перетаскивания
   let startX = 0;
   let startY = 0;
 
   onMount(() => {
+    loadImage();
     ctx = canvas.getContext("2d")!;
 
-    // Размеры canvas будут адаптироваться к размеру контейнера
+    // Адаптивный canvas
     const observer = new ResizeObserver((entries) => {
       const rect = entries[0].contentRect;
-
       width = rect.width;
       height = rect.height;
-
       canvas.width = width;
       canvas.height = height;
-
-      if (img) centerImage();
-
-      render();
     });
-
     observer.observe(canvas);
-
-    loadImage();
-
-    return () => observer.disconnect();
+    // Подписка на store 
+    unsubscribe = imageFile.subscribe((file) => {
+      if (!file) return;
+        loadImageFromFile(file);
+      });
+    return () => {
+      observer.disconnect();
+    };
+  });
+  onDestroy(() => {
+    unsubscribe?.();
   });
 
-  function loadImage() {
-    const file = get(imageFile);
-    if (!file) return;
-
+  function loadImageFromFile(file: File) {
+    // Очистка перед новой загрузкой
+    img = null;
+    ctx.clearRect(0, 0, width, height);
     const reader = new FileReader();
-
     reader.onload = () => {
       const image = new Image();
       image.src = reader.result as string;
-
       image.onload = () => {
         img = image;
-
         const channels = detectChannels(image);
-
-        onInfo?.({
+        imageInfo.set({
+          name: file.name,
           width: image.width,
           height: image.height,
           depth: channels,
         });
-
+        centerImage()
         render();
       };
     };
-
     reader.readAsDataURL(file);
   }
 
   function centerImage() {
     if (!img) return;
-
     offsetX = (width - img.width) / 2;
     offsetY = (height - img.height) / 2;
   }
@@ -85,7 +80,6 @@
   function render() {
     if (!ctx || !img) return;
     ctx.clearRect(0, 0, width, height);
-    // Вычисляем центрирование изображения
     ctx.drawImage(img, offsetX, offsetY);
   }
 
@@ -101,14 +95,12 @@
 
   function onMouseDown(e: MouseEvent) {
     isDragging = true;
-    // Вычисляем начальные координаты для перетаскивания
     startX = e.clientX - offsetX;
     startY = e.clientY - offsetY;
   }
 
   function onMouseMove(e: MouseEvent) {
     if (!isDragging || !img) return;
-    // Вычисляем новое смещение на основе движения мыши
     offsetX = e.clientX - startX;
     offsetY = e.clientY - startY;
     render();
