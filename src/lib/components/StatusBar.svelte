bc6ef428059d0c85a80098603b9dcd4c8b157a9d
alt1tla <GerdzhikovaVeronWench@gmail.com>
2026-04-17 11:27:43 +0300

    fix(statusbar): UI

--- src/lib/components/StatusBar.svelte
+++ src/lib/components/StatusBar.svelte
@@ -5,7 +5,7 @@
 <div
   class="bg-gray-900 border-t border-gray-700 px-4 py-2 text-sm text-gray-400"
 >
-  <div class="flex justify-between">
+  <div class="flex justify-end gap-2">
     <span>Ширина: {$imageInfo.width} px</span>
     <span>Высота: {$imageInfo.height} px</span>
     <span>Глубина цвета: {$imageInfo.depth} bpp</span>
